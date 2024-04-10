fba1cd07c10185e77bedb3e0d7e5b9343c6385c0
JunaPark831 <juna265@gmail.com>
2024-04-10 11:29:45 +0900

    variable type

--- src/Main.kt
+++ src/Main.kt
@@ -6,4 +6,26 @@ fun main() {
     println("oh sung-gong!!")
     println("push까지 해야 github에 반영됨!!")
     println("repeat")
+
+    val name : String = "Flature"
+    val sentence : String = "studying Kotlin"
+
+    val number1 = 123
+    val number = 123L
+    val number3 : Long = 123
+    val number4 : Double = 123.45
+    val number5 : Float = 123.45F
+    val number6 = 123.45
+
+    val isTrue = true
+    val isFalse : Boolean = false
+    var isFalse2 : Any = false
+    isFalse2 = true
+    isFalse2 = "can be String"
+    println(isFalse2)
+
+    val character = 'a'
+    println(character)
+    println(character + 1)
+
 }
